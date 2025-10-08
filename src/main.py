2d79bf5b8a6724ef16656c5e189abbc93f1983f3
robkisk <robby.kiskanyan@databricks.com>
2025-10-08 08:45:01 -0400

    Update .gitignore to exclude Python bytecode, modify pipeline catalog for deployment, and refactor main.py and notebook.ipynb for consistency and improved readability.

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ from pyspark.sql import DataFrame
 
 
 def find_all_taxis() -> DataFrame:
-    return spark.read.table("samples.nyctaxi.trips")
+  return spark.read.table('samples.nyctaxi.trips')
 
 
 def main():
-    find_all_taxis().show(5)
+  find_all_taxis().show(5)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  main()
